fb42b4a21bbd6bbf4ab114fea07b9083f88c6d5c
SanSanchezzz <13lex04@gmail.com>
2020-03-24 00:30:00 +0300

    Hello, world

---
+++ it_2/include/types.h
@@ -0,0 +1,16 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#define OK 0
+
+typedef struct position position_t;
+struct position
+{
+    int x;
+    int y;
+    int z;
+};
+
+
+
+#endif //TYPES_H
---
+++ it_2/source/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <types.h>
+
+int main()
+{
+    printf("Hello, world!\n");
+
+    return OK;
+}
